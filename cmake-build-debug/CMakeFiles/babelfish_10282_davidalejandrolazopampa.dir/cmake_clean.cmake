5057b89cd335b480fc4003158d581a3ddeb92b76
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2019-11-06 20:50:38 -0500

    Iniciando

---
+++ cmake-build-debug/CMakeFiles/babelfish_10282_davidalejandrolazopampa.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/babelfish_10282_davidalejandrolazopampa.dir/main.cpp.obj"
+  "babelfish_10282_davidalejandrolazopampa.exe"
+  "babelfish_10282_davidalejandrolazopampa.exe.manifest"
+  "babelfish_10282_davidalejandrolazopampa.pdb"
+  "libbabelfish_10282_davidalejandrolazopampa.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/babelfish_10282_davidalejandrolazopampa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
