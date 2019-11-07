2349fd8cc834bcc9e30357e9be35f9f9effea142
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2019-11-07 16:49:43 -0500

    Aceptado

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <map>
 using namespace std;
-int main(){
-    cout<<"Hola";
+int main() {
+    map<string, string> array;
+    string linea;
+    while ((getline(cin, linea)) && linea.length() > 0) {
+        int space = linea.find(' ');
+        auto a = linea.substr(0, space);
+        auto b = linea.substr(space + 1);
+        array[b] = a;
+    }
+    while ((getline(cin, linea)) && linea.length() > 0) {
+        if (array[linea].length() != 0)
+            cout << array[linea] << endl;
+        else
+            cout << "eh\n";
+    }
+    return 0;
 }
